97da711ba42498dc255870dc815041efb2d155d9
xmm <xiemei1996@163.com>
2019-11-22 22:12:54 +0800

    save

---
+++ src/views/trade/K.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="kbox">
+    <img src="../../assets/images/k.png" alt="" srcset="">
+  </section>
+</template>
+<script>
+export default {
+  name:'k'
+}
+</script>
+
+<style lang="scss" scoped>
+.kbox{
+  width: 100%;
+  img{
+    width: 100%;
+    height: 60vh;
+
+  }
+
+}
+</style>
+
--- src/views/trade/tradeDetails.vue
+++ src/views/trade/tradeDetails.vue
@@ -13,20 +13,32 @@
           <div><span>高</span><span>3573.86</span> </div>
           <div><span>低</span><span>3573.86</span></div>
           <div><span>24h</span><span>15678</span></div>
-
       </div>
+    </div>
+    <k></k>
+    <div class="type">
+      <div class="down">深度</div>
+      <div>市值</div>
+    </div>
+    <div class="busell">
+      <div class="buy">买入</div>
+      <div class="sell">卖出</div>
     </div>
   </section>
 </template>
 <script>
+import k from "./K"; //深度
 export default {
-  name: "tradeDetails"
+  name: "tradeDetails",
+   components: {
+    k
+   }
 };
 </script>
 <style lang="scss" scoped>
 .tradeDetails {
   
-  padding: 50px 0;
+  padding-top: 50px ;
   .toubox{
     padding: 0 15px;
     padding-bottom: 10px;
@@ -57,6 +69,40 @@ export default {
       }
     }
 
+  }
+  .type{
+    display: flex;
+    justify-content: space-between;
+   
+    div{
+      width: 50%;
+      height: 50px;
+      line-height: 45px;
+      font-size: 14px;
+      color:$color3;
+      text-align: center;
+    }
+  }
+  .busell{
+     border-top: 1px solid $border;
+     display: flex;
+     justify-content: space-between;
+     align-items: center;
+     padding: 10px 15px;
+    div{
+      width: 48%;
+      border-radius: 6px;
+      color: $white;
+      height: 44px;
+      line-height: 44px;
+      text-align: center
+    }
+    .buy{
+      background: $down;
+    }
+    .sell{
+      background: $up;
+    }
   }
 }
 </style>
