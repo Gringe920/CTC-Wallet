8170639af5fb1a09b1cbef5f360c04c1ec244c15
Lingzhu gu <lngzhu.gu@symbio.com>
2019-12-06 10:21:31 +0800

    替换图片、修改部分color

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,68 +1,65 @@
 <template>
-    <div class="btn-container" 
-         @click="comfirm"
-         :style="{width: width, height: height}">
-        <button :style="{background: bgColor}" class="bor-radio">
-            <i :class="icon" v-if="icon"></i>
-            {{text}}{{submitState ? '...' : ''}}
-        </button>
-    </div>
+  <div class="btn-container" @click="comfirm" :style="{width: width, height: height}">
+    <button :style="{background: bgColor}" class="bor-radio">
+      <i :class="icon" v-if="icon"></i>
+      {{text}}{{submitState ? '...' : ''}}
+    </button>
+  </div>
 </template>
 <script>
 export default {
-    props: {
-        submitState: false,
-        width: String,
-        height: String,
-        bgColor: String,
-        text: String,
-        icon: String
-    },
-    data(){
-        return {
-            
-        }
-    },
-    methods: {
-        comfirm(){
-            if(this.submitState) return;
-            this.$emit('comfirm')
-        }
+  props: {
+    submitState: false,
+    width: String,
+    height: String,
+    bgColor: String,
+    text: String,
+    icon: String
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    comfirm() {
+      if (this.submitState) return;
+      this.$emit("comfirm");
     }
-}
+  }
+};
 </script>
 <style lang="scss" scoped>
-.btn-container{
-    text-align: center;
-    margin: 0 auto;
-    i{
-        width: 22px;
-        height: 22px;
-        display: inline-block;
-        background-size: 100% 100%;
-        vertical-align: middle;
-        margin-right: 5px;
+.btn-container {
+  text-align: center;
+  margin: 0 auto;
+  i {
+    width: 22px;
+    height: 22px;
+    display: inline-block;
+    background-size: 100% 100%;
+    vertical-align: middle;
+    margin-right: 5px;
+  }
+  i.download_ios {
+    background-image: url("../assets/images/download_ios@2x.png");
+  }
+  i.download_android {
+    background-image: url("../assets/images/download_android@2x.png");
+  }
+  i.download_web {
+    background-image: url("../assets/images/download_web@2x.png");
+  }
+  button {
+    height: 46px;
+    color: $btnFontColor;
+    font-size: 14px;
+    width: 100%;
+    border: 0;
+    font-size: 16px;
+    background-color: $active;
+    font-weight: 500;
+    &.bor-radio {
+      border-radius: 32px;
     }
-    i.download_ios{
-        background-image: url('../assets/images/download_ios@2x.png');
-    }
-    i.download_android{
-        background-image: url('../assets/images/download_android@2x.png');
-    }
-    i.download_web{
-        background-image: url('../assets/images/download_web@2x.png');
-    }
-    button{
-        height: 46px;
-        color: $white;
-        font-size: 14px;
-        width: 100%;
-        border: 0;
-        font-size: 16px;
-        background-color: $active;
-        &.bor-radio{
-            border-radius: 32px;
-        }
-    }
+  }
 }
 </style>
